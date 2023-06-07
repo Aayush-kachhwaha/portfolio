ef34fab12e93648265ee3b74e3deb3de415fb7d7
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-06-07 17:39:14 +0530

    updates

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {BsLinkedin} from 'react-icons/bs';
-import {BsGithub} from 'react-icons/bs';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'> 
-        <a href="https://www.linkedin.com/in/aayush-kachhwaha/" target='_blank' rel="noreferrer"><BsLinkedin/></a>
-        <a href="https://github.com/Aayush-kachhwaha" target='_blank' rel="noreferrer"><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/aayush-kachhwaha/" target='_blank' rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/Aayush-kachhwaha" target='_blank' rel="noreferrer"><GitHubIcon/></a>
     </div>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './nav.css';
 
-import AiOutlineUser from 'react-icons/ai';
+
 
 
 function Navbar() {
   return (
     <nav>
-      <a href='#about'><AiOutlineUser/></a>
 
     </nav>
   )
