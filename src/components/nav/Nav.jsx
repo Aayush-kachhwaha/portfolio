2f547639001d7c7f860883fb781a936b67038f27
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-06-08 09:05:07 +0530

    updates

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,10 +12,10 @@ function Navbar() {
   return (
     <nav>
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
-      <a href='#about' onClick={() => setActiveNav('#about')}><AiOutlineUser/></a>
-      <a href='#experience'><BiBookBookmark/></a>
-      <a href='#services'><RiServiceLine/></a>
-      <a href='#contact'><BsFillChatLeftTextFill/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active':''}><AiOutlineUser/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active':''}><BiBookBookmark/></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active':''}><RiServiceLine/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active':''}><BsFillChatLeftTextFill/></a>
     </nav>
   )
 }
