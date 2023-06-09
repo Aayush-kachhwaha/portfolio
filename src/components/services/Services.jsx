097879602397fb14b8fbe920afbd9d9eb00e1812
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-06-10 00:04:43 +0530

    services updates

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,50 @@ import './services.css';
 
 function Services() {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I can Offer at my end</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>work</li>
+            <li>workwokr</li>
+            <li>eokr</li>
+            <li>owork</li>
+            <li>work</li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>work</li>
+            <li>workwokr</li>
+            <li>eokr</li>
+            <li>owork</li>
+            <li>work</li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>work</li>
+            <li>workwokr</li>
+            <li>eokr</li>
+            <li>owork</li>
+            <li>work</li>
+          </ul>
+        </article>
+      </div>
+
+    </section>
   )
 }
 
